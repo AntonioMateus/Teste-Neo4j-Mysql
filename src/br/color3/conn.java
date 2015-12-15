e237394b531d566322791c76eaa0e09a366882ba
AntonioMateus <antoniocms1995@hotmail.com>
2015-12-15 14:33:07 -0200

    Tentativa de conexão usando o JDBC

--- src/br/color3/conn.java
+++ src/br/color3/conn.java
@@ -5,46 +5,51 @@
  */
 package br.color3;
 //import java.sql.*; Mysql
-import iot.jcypher.database.DBAccessFactory;
-import iot.jcypher.database.DBProperties;
-import iot.jcypher.database.DBType;
-import iot.jcypher.database.IDBAccess;
-import iot.jcypher.graph.GrNode;
-import iot.jcypher.query.JcQuery;
-import iot.jcypher.query.JcQueryResult;
-import iot.jcypher.query.api.IClause;
-import iot.jcypher.query.factories.clause.MATCH;
-import iot.jcypher.query.factories.clause.RETURN;
-import iot.jcypher.query.values.JcNode;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
-import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.cypher.javacompat.ExecutionResult;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.neo4j.jdbc.Driver;
+
 
 public class conn{
     //Connection conn = null; Mysql
     	
-    public static IDBAccess connect(){
+    public static Connection connect() throws SQLException{
 	/* LOGICA ANTIGA 
         GraphDatabaseService conn = new GraphDatabaseFactory().newEmbeddedDatabase("C:/Users/Antonio Mateus/Desktop/Iniciação Cientifica/Iniciação - Fátima/Neo4J/neo4j-enterprise-2.3.0-M02/data/graphTest");
         //System.out.println("Conexao com o BD iniciada"); 
         return conn;*/
+        /* LOGICA MAIS NOVA
         Properties p = new Properties();
         p.setProperty(DBProperties.SERVER_ROOT_URI, "http://localhost:7476/");
-        IDBAccess remote = DBAccessFactory.createDBAccess(DBType.REMOTE, p, "neo4j", "antonio");
-        return remote;
+        IDBAccess remote = DBAccessFactory.createDBAccess(DBType.REMOTE, p, "neo4j", "admin");
+        return remote;*/
+        Connection conn = null;
+        try {
+            Class.forName("org.neo4j.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:neo4j://localhost:7476/","neo4j","admin"); 
+        }
+        catch (ClassNotFoundException c) {
+            System.out.println("Driver do Neo4j nao encontrado");
+        }
+        catch (SQLException s) {
+            System.out.println("Erro ao abrir a conexao");
+        }
+        return conn; 
     }
 		
-    public static void disconnect(IDBAccess conn){
+    public static void disconnect(Connection conn){
 	/* LOGICA ANTIGA
         conn.shutdown();
         //System.out.println("Conexao com o BD encerrada"); */
-        conn.close();
+        try {
+            conn.close();
+        }
+        catch (SQLException s) {
+            System.out.println("Erro ao fechar a conexao");
+        }
     }
     
     public static void main(String[] args) {
@@ -71,6 +76,7 @@ public class conn{
         finally {
             disconnect(conn);
         }*/
+        /* IDEIA MAIS NOVA 
         IDBAccess conn = connect(); 
         JcNode imagem = new JcNode("image");
         JcQuery consulta = new JcQuery(); 
@@ -89,6 +95,38 @@ public class conn{
         else {
             System.out.println("Nao existe ainda nenhuma imagem no banco de dados");
         }
-        disconnect(conn);
+        disconnect(conn);*/
+        String query = "optional match (n:image) return n";
+        Connection conn = null;
+        try {
+            conn = connect();
+            Statement stmt = conn.createStatement();
+            stmt.execute("match (n) optional match (n)-[r]-() delete n,r");
+            stmt.execute("create (teste:image {idImage:0, pathImage:'imagem/teste.jpg'})");
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next(); 
+            String first = rs.getString("n");
+            if (first == null) {
+                System.out.println("Nao ha nenhuma imagem no banco de dados");
+            }
+            else {
+                //System.out.println(first);
+                String[] paresPropriedadeValor = first.substring(1, first.length()-1).split(",");
+                for (String par: paresPropriedadeValor) {
+                    System.out.println(par.split(":")[0]+" = "+par.split(":")[1]);
+                }
+                while (rs.next()) {
+                    System.out.println(rs.getString("n"));
+                }
+            }
+        }
+        catch (SQLException s) {
+            System.out.println("Houve um problema durante a execucao de uma das consultas");
+        }
+        finally {
+            if (conn != null) {
+                disconnect(conn);
+            }
+        }
     }
 }
